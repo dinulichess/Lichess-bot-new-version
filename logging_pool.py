89efe72acf35f8babfb3e746ec4e60279bc0ec4b
RPNS Nimsilu <93962127+codingforhelp@users.noreply.github.com>
2022-02-18 14:43:23 +0530

    Update logging_pool.py

--- logging_pool.py
+++ logging_pool.py
@@ -2,13 +2,12 @@ import traceback
 from multiprocessing.pool import Pool
 import multiprocessing
 
-
 # Shortcut to multiprocessing's logger
 def error(msg, *args):
     return multiprocessing.get_logger().error(msg, *args)
 
 
-class LogExceptions:
+class LogExceptions(object):
     def __init__(self, callable):
         self.__callable = callable
 
@@ -16,7 +15,7 @@ class LogExceptions:
         try:
             result = self.__callable(*args, **kwargs)
 
-        except Exception:
+        except Exception as e:
             # Here we add some debugging help. If multiprocessing's
             # debugging is on, it will arrange to log the traceback
             error(traceback.format_exc())
